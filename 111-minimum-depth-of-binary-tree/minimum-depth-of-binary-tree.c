b91f5746ee01caa787e27f450ab8c2278320b8b9
Cebelle <84433822+Cebelle1@users.noreply.github.com>
2024-02-19 22:49:59 +0800

    Time: 156 ms (14.05%) | Memory: 76.7 MB (85.58%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.c
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.c
@@ -7,18 +7,11 @@
  * };
  */
 int minDepth(struct TreeNode* root) {
-    if(root == NULL) return 0;
+    if(root== NULL)return 0;
     int left = minDepth(root->left);
     int right = minDepth(root->right);
-    if(root->left == NULL && root->right == NULL) return 1;
-    if(root->left == NULL) return 1 + right;
-    if(root->right == NULL) return 1 + left;
-    return right>left?left+1:right+1;
-    
-
-    if(left < right){
-        return left + 1;
-    }else{
-        return right + 1;
-    }
+    if (root->left == NULL && root->right == NULL) return 1;
+    if(root->left == NULL) return 1+right;  //if left child is empty return right child depth
+    if(root->right == NULL) return 1+left; //if right child is empty return left child depth
+    return left<right? left +1: right +1; //after both child return, choose the smallest and add self
 }
