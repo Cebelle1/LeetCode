06b71d5c2bf9dbcf397054efaec246bbbc834740
Loo Si Hui <84433822+Cebelle1@users.noreply.github.com>
2025-08-29 17:25:04 +0800

    Time: 2 ms (39.33%) | Memory: 77.1 MB (73.73%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -8,38 +8,33 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+#include <cmath>
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-       // Create a new linked list to store result in
-       ListNode* calc = new ListNode(0); //init as 0
-       ListNode* header = calc; //Header to point to calc linked list
+        // Add them node by node and do carry over
+        ListNode* dummy = new ListNode();
+        ListNode* res = dummy;
+        int total =0, carry =0;
 
-        int carry = 0; // Create a carry variable to account for carry over
-
-       // Loop through the non-empty linked lists
-       // & Loop when carry not 0 to continue adding
-       while(l1 != nullptr || l2 != nullptr || carry != 0){
-
-            int sum = carry;
-
-            if(l1 != nullptr){
-                sum += l1->val;
-                l1 = l1->next;
+        while(l1 || l2 || carry){
+            if(l1){
+                total+=l1->val;
+                l1=l1->next;
             }
 
-            if(l2 != nullptr){
-                sum += l2->val;
-                l2 = l2->next;
+            if(l2){
+                total+= l2->val;
+                l2=l2->next;
             }
-
-            carry = sum/10;
-            header->next = new ListNode(sum % 10);
-            header = header->next;
-
-       }
-
-       ListNode* result = calc->next;
-       return result;
+            int num = total%10; //get the ones place
+            carry = total/10;
+            ListNode* newNode = new ListNode(num);
+            dummy->next = newNode;
+            dummy=dummy->next;
+            total = carry;
+        }
+        res=res->next;
+        return res;
     }
 };
