11e370fe1f4eadffcdebdc3e6c8779127319128b
Loo Si Hui <84433822+Cebelle1@users.noreply.github.com>
2025-08-29 15:46:23 +0800

    Time: 0 ms (100.00%) | Memory: 14.7 MB (50.41%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,21 +4,16 @@ public:
         // Two-pass hash table
         // Convert vector into hash map
         unordered_map<int, int> hash;
+        //While iterating, lets compute also
         for(int i=0; i< nums.size(); i++){
-            // Since later .find(complement), should store nums[i] so .find() finds nums[i]
+            int complement = target - nums[i];
+            if(hash.find(complement) != hash.end()){
+                return {hash[complement], i};
+            }
             hash[nums[i]] = i;
             //std::cout << nums[i]<< ":" << hash[nums[i]] << std::endl;
         }
 
-        //Find the complement and check hash map in O(1)
-        // This makes it O(n)
-        for(int i=0; i< nums.size(); i++){
-            int complement = target - nums[i];
-            // Check if complement exists
-            if(hash.find(complement) != hash.end() && hash[complement] != i){
-                return {hash[complement], i};
-            }
-        }
         return {};
     }
 };
