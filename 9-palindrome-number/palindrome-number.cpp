d47a23933f658810f3c226417395ca485ab3b7e8
Loo Si Hui <84433822+Cebelle1@users.noreply.github.com>
2025-08-31 13:45:03 +0800

    Time: 3 ms (45.31%) | Memory: 8.5 MB (92.05%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        //Negative numbers are not palindromes
-        if( x<0) return false;
-        stack<char> s;
-        string xStr = to_string(x);
         
-        for(int i=0; i<xStr.length(); i++){
-            s.push(xStr[i]);
+        //Negative numbers && nums ending with 0 are not palindromes
+        if(x < 0 || x%10 == 0 && x!=0)  return false;
+        if(x/10 == 0) return true; //single digits are palindromes
+        long reversed = 0;
+        int xx = x;
+        while(xx !=0){
+            reversed = reversed * 10 + xx%10;
+            xx/=10;
         }
-        for(int i=0; i<xStr.length(); i++){
-            char compare = s.top();
-            s.pop();
-            if(compare != xStr[i]) return false;
-        }
-       
 
-        return true;
+        return x == reversed;
     }
 };
