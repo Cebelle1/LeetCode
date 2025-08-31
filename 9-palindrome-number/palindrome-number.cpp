d5a026a9af17861214650652f41dd4eae7feba9b
Loo Si Hui <84433822+Cebelle1@users.noreply.github.com>
2025-08-31 13:45:24 +0800

    Time: 0 ms (100.00%) | Memory: 8.7 MB (40.82%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        
         //Negative numbers && nums ending with 0 are not palindromes
         if(x < 0 || x%10 == 0 && x!=0)  return false;
-        if(x/10 == 0) return true; //single digits are palindromes
+         //single digits are palindromes
+        if(x/10 == 0) return true;
+        
         long reversed = 0;
         int xx = x;
         while(xx !=0){
             reversed = reversed * 10 + xx%10;
             xx/=10;
         }
-
         return x == reversed;
     }
 };
