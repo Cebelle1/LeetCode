491e384952139b95b3af5d8207bd526bc7dfeb15
Loo Si Hui <84433822+Cebelle1@users.noreply.github.com>
2025-09-01 23:36:34 +0800

    Time: 297 ms (5.06%) | Memory: 366 MB (5.01%) - LeetSync

---
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        if (head == NULL) return NULL;
+        ListNode* cur = head;
+        vector<int> nums;
+        while(cur){
+            nums.push_back(cur->val);
+            cur=cur->next;
+        }
+        return build(nums, 0, nums.size()-1);
+    }
+
+    TreeNode* build(vector<int> nums, int left, int right){
+        if(left>right){
+            return NULL;
+        }
+        int mid = (left+right)/2;
+        TreeNode* node = new TreeNode(nums[mid]);
+        node->left = build(nums, left, mid-1);
+        node->right = build(nums, mid+1, right);
+        return node;
+    }
+};
